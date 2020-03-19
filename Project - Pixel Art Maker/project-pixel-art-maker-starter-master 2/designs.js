554de35f4cfb198280e689588831e4a7447a9887
Chris <clearwhitesky@outlook.com>
2020-03-19 19:07:46 +0800

    inital ver

--- Project - Pixel Art Maker/project-pixel-art-maker-starter-master 2/designs.js
+++ Project - Pixel Art Maker/project-pixel-art-maker-starter-master 2/designs.js
@@ -1,10 +1,30 @@
 // Select color input
+var makeGridColor;
+
 // Select size input
 
+var makeGridHeight;
+var makeGridWidth;
+
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    makeGridHeight = $('#inputHeight').val();
+    makeGridWidth = $('#inputWidth').val();
+    makeGridColor = $('colorPicker').val();
+    makeGrid(makeGridHeight, makeGridWidth)
+})
 
-function makeGrid() {
 
-// Your code goes here!
+function makeGrid(makeGridHeight, makeGridWidth) {
+    $('tr').remove();
+    for (var i = 1; i <= makeGridHeight; i++) {
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');  
+        for (var j = 1; j <= makeGridWidth; j++) {
+            $('#table' + i).append('<td></td>');
+        }
+        $(this).click(function(event) {$(this).attr('style', 'background-color:' + makeGridColor().val);
+    }
 
+    }
 }
